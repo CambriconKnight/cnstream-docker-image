b13a5ed85123c0e887d23aec44ab5b2d75875ffa
kang <kangshaopeng@cambricon.com>
2021-04-06 14:17:15 +0800

    update to 1.6.602

---
+++ build-image-ubuntu16.04-cnstream.sh
@@ -0,0 +1,106 @@
+#/bin/bash
+set -e
+# -------------------------------------------------------------------------------
+# Filename:     build-image-ubuntu16.04-cnstream.sh
+# UpdateDate:   2021/02/23
+# Description:  Build docker images for cnstream.
+# Example:      ./build-image-ubuntu16.04-cnstream.sh
+# Depends:
+#               driver(ftp://download.cambricon.com:8821/product/MLU270/1.6.602/driver/neuware-mlu270-driver-dkms_4.9.2_all.deb)
+#               cntoolkit(ftp://download.cambricon.com:8821/product/MLU270/1.6.602/cntoolkit/X86_64/ubuntu16.04/cntoolkit_1.7.2-1.ubuntu16.04_amd64.deb)
+#               cnstream(https://gitee.com/SolutionSDK/CNStream.git)
+#               ffmpeg(https://gitee.com/mirrors/ffmpeg.git)
+# Notes:
+# -------------------------------------------------------------------------------
+#################### function ####################
+help_info() {
+    echo "
+Build docker images for cnstream.
+Usage:
+    $0 <command> [arguments]
+The commands are:
+    -h      Help info.
+    -m      MLU Platform.(mlu270, mlu220m.2)
+Examples:
+    $0 -h
+    $0 -m mlu270
+    $0 -m mlu220m.2
+Use '$0 -h' for more information about a command.
+    "
+}
+
+# Refresh global variables
+refresh_global_variables() {
+    #UPPERCASE:mlu270--->MLU270
+    MLU_Platform=`echo ${MLU} | tr '[a-z]' '[A-Z]'`
+    if [[ "${MLU_Platform}" == "MLU270" ]] || [[ "${MLU_Platform}" == "MLU220M.2" ]] ; then
+        MLU_Platform="MLU270"
+    else
+        help_info
+    fi
+}
+
+#################### main ####################
+# Source env
+source "./env.sh"
+
+#MLU Platform
+MLU="mlu270"
+#neuware or cntoolkit
+neuware_package_name="cntoolkit_1.7.2-1.ubuntu16.04_amd64.deb"
+
+# Get parameters
+while getopts "h:m:" opt; do
+    case $opt in
+    h) help_info  &&  exit 0
+        ;;
+    m) MLU="$OPTARG"
+        ;;
+    \?)
+        help_info && exit 0
+        ;;
+    esac
+done
+
+#Refresh global variables
+refresh_global_variables
+
+##0.git clone
+if [ ! -d "${PATH_WORK}" ];then
+    #git clone https://github.com/Cambricon/cnstream.git
+    git clone https://gitee.com/SolutionSDK/CNStream.git
+    mv CNStream ${PATH_WORK}
+else
+    echo "Directory($PATH_WORK): Exists!"
+fi
+cd "${PATH_WORK}"
+git submodule  update  --init
+# del .git
+#find . -name ".git" | xargs rm -Rf
+
+##copy the dependent packages into the directory of $PATH_WORK
+if [ -f "${neuware_package_name}" ];then
+    echo "File(${neuware_package_name}): Exists!"
+else
+    echo -e "${red}File(${neuware_package_name}): Not exist!${none}"
+    echo -e "${yellow}1.Please download ${neuware_package_name} from FTP(ftp://download.cambricon.com:8821/***)!${none}"
+    echo -e "${yellow}  For further information, please contact us.${none}"
+    echo -e "${yellow}2.Copy the dependent packages(${neuware_package_name}) into the directory!${none}"
+    echo -e "${yellow}  eg:cp -v /data/ftp/mlu270/$VER/cntoolkit/X86_64/ubuntu16.04/${neuware_package_name} ./${PATH_WORK}${none}"
+    #Manual copy
+    #cp -v /data/ftp/mlu270/1.6.602/cntoolkit/X86_64/ubuntu16.04/cntoolkit_1.4.110-1.ubuntu16.04_amd64.deb ./ffmpeg-mlu
+    exit -1
+fi
+
+#1.build image
+echo "====================== build image ======================"
+sudo docker build -f ../$FILENAME_DOCKERFILE \
+    --build-arg neuware_package=${neuware_package_name} \
+    --build-arg mlu_platform=${MLU_Platform} \
+    -t $NAME_IMAGE .
+#2.save image
+echo "====================== save image ======================"
+sudo docker save -o $FILENAME_IMAGE $NAME_IMAGE
+mv $FILENAME_IMAGE ../
+cd ../
+ls -la $FILENAME_IMAGE
---
+++ env.sh
@@ -0,0 +1,53 @@
+# -------------------------------------------------------------------------------
+# Filename:    env.sh
+# Revision:    1.0.0
+# Date:        2021/02/23
+# Description: Common Environment variable
+# Example:
+# Depends:
+# Notes:
+# -------------------------------------------------------------------------------
+#Work
+PATH_WORK="cnstream"
+#Dockerfile(16.04/18.04/CentOS)
+TYPE_DOCKERFILE="16.04"
+#(Dockerfile.16.04/Dockerfile.18.04/Dockerfile.CentOS)
+FILENAME_DOCKERFILE="Dockerfile.$TYPE_DOCKERFILE"
+DIR_DOCKER="docker"
+#Version
+VER="1.6.602"
+VERSION="v1.6.602"
+#VERSION="v${VER}"
+#VERSION="iva-1.6.106"
+#Organization
+ORG="cam"
+#Operating system
+OS="ubuntu16.04"
+#Docker image(cam/ubuntu16.04-$PATH_WORK)
+MY_IMAGE="$ORG/$OS-$PATH_WORK"
+#Docker image name(cam/ubuntu16.04-$PATH_WORK:v1.6.0)
+NAME_IMAGE="$MY_IMAGE:$VERSION"
+#FileName DockerImage(image-ubuntu16.04-$PATH_WORK-v1.6.0.tar.gz)
+FILENAME_IMAGE="image-$OS-$PATH_WORK-$VERSION.tar.gz"
+FULLNAME_IMAGE="./${FILENAME_IMAGE}"
+#Docker container name(container-ubuntu16.04-caffe-v1.6.0)
+MY_CONTAINER="container-$OS-$PATH_WORK-$VERSION"
+
+#Font color
+none="\033[0m"
+black="\033[0;30m"
+dark_gray="\033[1;30m"
+blue="\033[0;34m"
+light_blue="\033[1;34m"
+green="\033[0;32m"
+light_green="\033[1;32m"
+cyan="\033[0;36m"
+light_cyan="\033[1;36m"
+red="\033[0;31m"
+light_red="\033[1;31m"
+purple="\033[0;35m"
+light_purple="\033[1;35m"
+brown="\033[0;33m"
+yellow="\033[1;33m"
+light_gray="\033[0;37m"
+white="\033[1;37m"
